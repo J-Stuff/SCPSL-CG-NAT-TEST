7e3a04f31119d4710d4245433af076961009f3b2
J-Stuff <jstuff@j-stuff.net>
2023-06-01 12:59:09 +1200

    update update-pdm

--- __update-pdm.bat
+++ __update-pdm.bat
@@ -1,4 +1,4 @@
-@REM This program quickly updates PDM to use the requirements.txt file. Not needed for running the bot, just a script to make developing it easier
+@REM This program quickly updates PDM to use the requirements.txt file. Not needed for running the program, just a script to make developing it easier
 @echo off
 pdm import -f requirements requirements.txt
 pdm export -f requirements -o ./requirements.txt --without-hashes
